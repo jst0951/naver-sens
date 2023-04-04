ea3e0f14880249efc2854e1a40e2ece9350c0620
jst0951 <jst0951@gmail.com>
2023-04-05 02:52:32 +0900

    initial commit

---
+++ naver_sens/__init__.py
@@ -0,0 +1,6 @@
+"""
+Reference : https://api.ncloud-docs.com/docs/ai-application-service-sens-smsv2\n
+Guide : https://codedbyjst.tistory.com/\n
+Naver Cloud Platform SENS(Simple & Easy Notification Service) python wrapper\n
+"""
+from .naver_sens import SensClient
